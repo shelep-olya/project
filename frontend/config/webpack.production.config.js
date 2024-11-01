38cdd2a1b17c14a17e36b35ebd5e0bd1a15b029e
shelep-olya <shelep.olya@gmail.com>
2024-11-01 11:11:19 +0200

    changed config for deployment

--- frontend/config/webpack.production.config.js
+++ frontend/config/webpack.production.config.js
@@ -1,31 +1,61 @@
-const { merge } = require('webpack-merge');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const basicConfig = require('./webpack.config');
+const { merge } = require("webpack-merge");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const basicConfig = require("./webpack.config");
+const webpack = require("webpack");
+const dotenv = require("dotenv");
+
+dotenv.config();
 
 const config = {
-    mode: 'production',
-    plugins: [new MiniCssExtractPlugin()],
+    mode: "production",
     module: {
         rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader",
+                    options: {
+                        presets: ["@babel/preset-env", "@babel/preset-react"],
+                    },
+                },
+            },
             {
                 test: /\.css$/i,
                 use: [
                     MiniCssExtractPlugin.loader,
                     {
-                        loader: 'css-loader',
+                        loader: "css-loader",
                         options: {
                             modules: {
-                                mode: 'local',
-                                localIdentName: '[name]__[local]',
-                                // namedExport: true, case 1;
+                                mode: "local",
+                                localIdentName: "[name]__[local]--[hash:base64:5]",
                                 namedExport: false,
                             },
                         },
                     },
                 ],
             },
+            {
+                test: /\.(ico|png|jpg|jpeg|svg)$/i,
+                type: "asset/resource",
+                generator: {
+                    filename: "assets/[name][ext]",
+                },
+            },
         ],
     },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: "[name].[contenthash].css", // Output filename for CSS
+        }),
+        new webpack.DefinePlugin({
+            "process.env": {
+                MAIN_TEST_ID: JSON.stringify(process.env.MAIN_TEST_ID),
+                REACT_APP_API_URL: JSON.stringify(process.env.REACT_APP_API_URL),
+            },
+        }),
+    ],
 };
 
 module.exports = merge(basicConfig, config);
